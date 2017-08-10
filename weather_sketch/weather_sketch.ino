97bdfb2be2ec4f178131ad3916f511193d9f5186
shyward1 <shy.ward@gmail.com>
2017-08-09 19:07:54 -0500

    working tempature wih DS18B20

--- weather_sketch/weather_sketch.ino
+++ weather_sketch/weather_sketch.ino
@@ -1,13 +1,1 @@
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);  
-  Serial.println("--- Start Serial Monitor SEND_RCVE ---");
-  Serial.println(" Type in Box above, . ");
-  Serial.println("(Decimal)(Hex)(Character)");  
-  Serial.println(); 
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  //Serial.println("hello arduino");
-}
